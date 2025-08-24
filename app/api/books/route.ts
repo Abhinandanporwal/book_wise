38cdb59516cbb33b245f48cf84caefeb7f5b4809
Abhinandanporwal <145247297+Abhinandanporwal@users.noreply.github.com>
2025-08-25 02:30:33 +0530

    Update route.ts

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,16 +1,11 @@
-import { NextResponse } from "next/server";
-import { db } from "@/lib/prisma";
+import { NextResponse } from "next/server"
+import { db } from "@/lib/prisma"
 
 export async function GET() {
   try {
-    const books = await db.book.findMany();
-    return NextResponse.json(books, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching books:", error);
-
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+    const books = await db.book.findMany()
+    return NextResponse.json(books)
+  } catch{
+    return NextResponse.json({ error: "Failed to fetch books" }, { status: 500 })
   }
 }
